717018a16cb57db5802e37cdcbe3e1567ab73f21
gkaran <gkaran555@gmail.com>
2018-01-13 16:02:14 +0100

    scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,37 @@
-;(function(){
 
-			// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
+$(document).ready(function(){
+	
+  // Add smooth scrolling to all links
+  $(".smoothScroll").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 
 
-})(jQuery)
+  // Menu settings
+  $('#menuToggle, .menu-close').on('click', function(){
+	$('#menuToggle').toggleClass('active');
+	$('body').toggleClass('body-push-toleft');
+	$('#theMenu').toggleClass('menu-open');
+  });
+
+
+
+});
